dc4379e1fbe8fe78994eb57f8b64cf4ca7c406dc
onishi.kohei <kohei.o2222@gmail.com>
2022-07-03 22:07:04 +0900

    258のA

---
+++ ABC/258/A.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+
+const to = (str: any) => console.log(str);
+class Input {
+    private idx: number;
+    private arr: string[];
+
+    constructor() {
+        this.idx = 0;
+        this.arr = fs.readFileSync('/dev/stdin', 'utf8').split(/\s/);
+    }
+
+    word = () => this.arr[this.idx++];
+    words = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n);
+        this.idx += n;
+        return res;
+    };
+
+    number = () => Number(this.arr[this.idx++]);
+    numbers = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(s => +s);
+        this.idx += n;
+        return res;
+    };
+
+    bigint = () => BigInt(this.arr[this.idx++]);
+    bigints = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(BigInt);
+        this.idx += n;
+        return res;
+    };
+}
+
+const main = () => {
+    const input = new Input();
+
+    const K = input.number();
+    let hour = Math.floor(K / 60);
+    let min = ('0' + (K % 60)).slice(-2);
+    const time = 21 + hour;
+    to(`${time}:${min}`);
+};
+main();
