7d21cb865c0d7bf90d93d7c607795ffc0e230b86
onishi.kohei <kohei.o2222@gmail.com>
2022-07-03 11:36:50 +0900

    ２５７のA

---
+++ ABC/257/A.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+
+const to = (str: any) => console.log(str);
+class Input {
+    private idx: number;
+    private arr: string[];
+
+    constructor() {
+        this.idx = 0;
+        this.arr = fs.readFileSync('/dev/stdin', 'utf8').split(/\s/);
+    }
+
+    word = () => this.arr[this.idx++];
+    words = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n);
+        this.idx += n;
+        return res;
+    };
+
+    number = () => Number(this.arr[this.idx++]);
+    numbers = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(s => +s);
+        this.idx += n;
+        return res;
+    };
+
+    bigint = () => BigInt(this.arr[this.idx++]);
+    bigints = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(BigInt);
+        this.idx += n;
+        return res;
+    };
+}
+
+const main = () => {
+    const input = new Input();
+    const [N, X] = input.numbers(2);
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let str = alphabet.split('').reduce((sum, cur): string => {
+        return sum + cur.repeat(N);
+    }, '');
+
+    to(str[X - 1]);
+};
+
+main();
