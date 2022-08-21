26068a5fe474e24f153bd02edc6316cf1f404c2e
onishi.kohei <kohei.o2222@gmail.com>
2022-08-21 22:57:47 +0900

    265A追加

---
+++ ABC/265/A.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs';
+const to = (str: any) => console.log(str);
+class Input {
+    private idx: number;
+    private arr: string[];
+
+    constructor() {
+        this.idx = 0;
+        this.arr = fs.readFileSync('/dev/stdin', 'utf8').split(/\s/);
+    }
+
+    word = () => this.arr[this.idx++];
+    words = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n);
+        this.idx += n;
+        return res;
+    };
+
+    number = () => Number(this.arr[this.idx++]);
+    numbers = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(s => +s);
+        this.idx += n;
+        return res;
+    };
+
+    bigint = () => BigInt(this.arr[this.idx++]);
+    bigints = (n: number) => {
+        const res = this.arr.slice(this.idx, this.idx + n).map(BigInt);
+        this.idx += n;
+        return res;
+    };
+}
+
+const main = () => {
+    const input = new Input();
+
+    let [X, Y, N] = input.numbers(3);
+    if (X * 3 <= Y) {
+        to(N * X);
+    } else {
+        let n = N % 3;
+        to(X * n + ((N - n) / 3) * Y);
+    }
+};
+main();
